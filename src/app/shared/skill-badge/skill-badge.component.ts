d74ba2b892c6bd68110a643f0ed6aa43eb67e9bd
Tony-Muriuki <kamandetonymuriuki@gmail.com>
2025-04-14 13:28:14 +0300

    Initial Commit

---
+++ src/app/shared/skill-badge/skill-badge.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-skill-badge',
+  imports: [],
+  templateUrl: './skill-badge.component.html',
+  styleUrl: './skill-badge.component.scss'
+})
+export class SkillBadgeComponent {
+
+}
